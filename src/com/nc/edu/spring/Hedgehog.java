a4c09226df699e63d7dd5170f5dfdc158e9e53a7
Winter2016 <tsmgofm@mail.ru>
2016-03-13 21:08:32 +0300

    Задание 3.

--- src/com/nc/edu/spring/Hedgehog.java
+++ src/com/nc/edu/spring/Hedgehog.java
@@ -7,21 +7,7 @@ import java.util.*;
  */
 public class Hedgehog {
 
-    private List<Needle> needles;
-    private Set<String> childrenNames;
     private Map<String, String> familyNames;
-
-    public void setNeedles(List<Needle> needles) {
-        this.needles = needles;
-        System.out.println("Hedgehog.setNeedles");
-        System.out.println("needles = " + needles);
-    }
-
-    public void setChildrenNames(Set<String> childrenNames) {
-        this.childrenNames = childrenNames;
-        System.out.println("Hedgehog.setChildrenNames");
-        System.out.println("childrenNames = " + childrenNames);
-    }
 
     public void setFamilyNames(Map<String, String> familyNames) {
         this.familyNames = familyNames;
