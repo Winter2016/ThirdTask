cdd20582fe986a978b787f6776414b791ddf39d0
Winter2016 <tsmgofm@mail.ru>
2016-03-13 19:04:07 +0300

    Задание 1 с SI.

--- src/com/nc/edu/spring/Hedgehog.java
+++ src/com/nc/edu/spring/Hedgehog.java
@@ -6,9 +6,33 @@ package com.nc.edu.spring;
 public class Hedgehog {
     private Nose nose;
     private int age;
-    public Hedgehog(Nose nose, int age)
+   /* public Hedgehog(Nose nose, int age)
     {
         this.nose = nose;
+        this.age = age;
+        System.out.println("Hedgehog.Hedgehog");
+        System.out.println("nose = " + nose);
+        System.out.println("age = " + age);
+        System.out.println("this = " + this);
+    }*/
+
+    public Nose getNose() {
+        return nose;
+    }
+
+    public void setNose(Nose nose) {
+        this.nose = nose;
+        System.out.println("Hedgehog.Hedgehog");
+        System.out.println("nose = " + nose);
+        System.out.println("age = " + age);
+        System.out.println("this = " + this);
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
         this.age = age;
         System.out.println("Hedgehog.Hedgehog");
         System.out.println("nose = " + nose);
