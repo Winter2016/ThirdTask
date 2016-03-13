913a4795920fd6b443ff2afeb1f6abf587a7122c
Winter2016 <tsmgofm@mail.ru>
2016-03-14 00:26:59 +0300

    Задание 7.

--- src/com/nc/edu/spring/Hedgehog.java
+++ src/com/nc/edu/spring/Hedgehog.java
@@ -5,17 +5,7 @@ import java.util.*;
 /**
  * Created by Ксения on 3/13/2016.
  */
-public class Hedgehog {
+public abstract class Hedgehog {
     private Nose nose = new Nose();
-
-    public Hedgehog() {
-        System.out.println("Hedgehog.setNose");
-        System.out.println("nose = " + nose);
-        System.out.println("this = " + this);
-    }
-
-    public Nose createNose() {
-        System.out.println("Hedgehog.createNose");
-        System.out.println("nose = " + nose);
-        return nose;}
+    protected abstract Nose createNose();
 }
